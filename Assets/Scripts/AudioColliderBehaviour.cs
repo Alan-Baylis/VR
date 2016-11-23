16f3dd2ac514b09b0e29ceb0acb6fe4eba8547c7
Arno <arno.lehtonen@gmail.com>
2016-11-23 18:44:18 +0200

    Rotating symbol, particle adjustments, fadein/out, playOnlyOnce.

--- Assets/Scripts/AudioColliderBehaviour.cs
+++ Assets/Scripts/AudioColliderBehaviour.cs
@@ -3,25 +3,59 @@ using System.Collections;
 
 public class AudioColliderBehaviour : MonoBehaviour {
 
-    void OnTriggerEnter (Collider other)
+    [Tooltip("Use with caution: counts only, when player has entered the volume.")]
+    public bool playsOnlyOnce = false;
+    private bool hasBeenPlayed = false;
+
+    // Use this for initialization
+    void Start()
     {
-        Debug.Log("There was somethign here");
-        gameObject.GetComponent<AudioSource>().Play();
 
     }
 
+    void OnTriggerEnter (Collider other) {
+
+        if (playsOnlyOnce && !hasBeenPlayed)
+        {
+            StartCoroutine(FadeAudioIn());
+            hasBeenPlayed = true;
+        } else if (playsOnlyOnce && hasBeenPlayed) {
+            Debug.Log("Tämä pätkä soiteltiin jo.");
+        } else
+        {
+           StartCoroutine(FadeAudioIn());
+        }
+        
+    }
+    
     void OnTriggerExit (Collider other)
     {
         Debug.Log("Something exited the trigger volume.");
-        gameObject.GetComponent<AudioSource>().Pause();
-
+        StartCoroutine(FadeAudioOut());
 
     }
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+
+    IEnumerator FadeAudioOut()
+    {
+        while(GetComponent<AudioSource>().volume > 0.0f)
+        {
+            GetComponent<AudioSource>().volume -= 0.02f;
+            yield return new WaitForEndOfFrame();
+        }
+
+        GetComponent<AudioSource>().Pause();
+    }
+
+    IEnumerator FadeAudioIn()
+    {
+        gameObject.GetComponent<AudioSource>().volume = 0;
+        GetComponent<AudioSource>().Play();
+        while (GetComponent<AudioSource>().volume < 1.0f)
+        {
+            GetComponent<AudioSource>().volume += 0.05f;
+            yield return new WaitForEndOfFrame();
+        }  
+    }
 	// Update is called once per frame
 	void Update () {
 	
