2b9b9c648d2bec97f20070ad84ac87492ea79f2a
Arno <arno.lehtonen@gmail.com>
2016-11-23 14:59:34 +0200

    Audio Triggers with pause func. Some problems with particles that are supposed to resemble the audio area. Very much WIP.

---
+++ Assets/Scripts/AudioColliderBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioColliderBehaviour : MonoBehaviour {
+
+    void OnTriggerEnter (Collider other)
+    {
+        Debug.Log("There was somethign here");
+        gameObject.GetComponent<AudioSource>().Play();
+
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        Debug.Log("Something exited the trigger volume.");
+        gameObject.GetComponent<AudioSource>().Pause();
+
+
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
