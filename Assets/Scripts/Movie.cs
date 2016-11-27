3f1211b3a047c6ed69c40d0da85f1c5cbe0deff3
Ari Marin <ari.marin@oscar.fi>
2016-11-28 00:13:44 +0200

    Added 360 video player prefab

---
+++ Assets/Scripts/Movie.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movie : MonoBehaviour {
+
+    public MovieTexture Video;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Renderer>().material.mainTexture = Video;
+        Video.Play();
+    }
+}
