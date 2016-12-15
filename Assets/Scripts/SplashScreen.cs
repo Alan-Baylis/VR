4baac9a183df9e36fb72007cd74b6e3bce19d659
Arno <arno.lehtonen@gmail.com>
2016-12-15 13:36:30 +0200

    A simple splash screen to start things off with.

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.SceneManagement;
+
+namespace VRTK
+{
+    public class SplashScreen : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            StartCoroutine(MoveToNextScene());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        IEnumerator MoveToNextScene()
+        {
+            yield return new WaitForSeconds(5);
+            GetComponent<SteamVR_LoadLevel>().Trigger();
+
+        }
+    }
+}
