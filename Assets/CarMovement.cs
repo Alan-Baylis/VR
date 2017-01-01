dba1c014950b007f4aa0bb91c8c2f3cf82deef60
Ville Nukarinen <ville.nukarinen@gmail.com>
2017-01-01 23:23:53 +0200

    Add car movement script

---
+++ Assets/CarMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarMovement : MonoBehaviour {
+    float speed;
+    float progress;
+    Vector3 current;
+    Vector3 target;
+
+    // Use this for initialization
+    void Start () {
+        current = this.transform.position;
+        target = GameObject.Find("wp1").transform.position;
+        speed = 0.01f;
+        progress = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(this.transform.position);
+        progress += speed * Time.deltaTime;
+
+        this.transform.LookAt(target);
+        this.transform.position = Vector3.Slerp(current, target, progress);
+	}
+}
