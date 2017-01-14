04c6a79e422b3510728799d9c5c9511613287056
Ville Nukarinen <ville.nukarinen@gmail.com>
2017-01-14 19:45:19 +0200

    Improves lens flares - Script to scale lens flare brightness based on distance to player - Allows maximum distance and exponential dropoff
    Fixes #4

---
+++ Assets/LensFlareDistanceScale.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LensFlareDistanceScale : MonoBehaviour {
+
+    GameObject player;
+    LensFlare subject;
+
+    // Public interface
+    public float exponentiality = 1f;
+    public float brightness = 1f;
+    public float minimumBrightness = 0.1f;
+    public float maximumDistance = 500f;
+
+	// Use this for initialization
+	void Start () {
+	    player = GameObject.FindWithTag("Player");
+        subject = this.GetComponent<LensFlare>();
+        subject.fadeSpeed = brightness;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float distance = Vector3.Distance(player.transform.position, this.transform.position);
+        float brightnessLevel = Mathf.Pow((brightness / distance), exponentiality);
+
+        // If exponential value is smaller than minimum brightness, then minimum brightness is used and scaled to maximum distance.
+        if (brightnessLevel < minimumBrightness) {
+            brightnessLevel = Mathf.Max(0f, minimumBrightness * (maximumDistance - distance) / maximumDistance);
+        }
+
+        subject.brightness = brightnessLevel;
+    }
+}
