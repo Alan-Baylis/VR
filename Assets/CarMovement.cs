c60e4d1d3eb53cae6adbcec6b1c82d2abcc3df95
Ville Nukarinen <ville.nukarinen@gmail.com>
2017-01-14 18:06:48 +0200

    Fix sliding car and put guiding arrows under environment in road scene
    Fixes #5

--- Assets/CarMovement.cs
+++ Assets/CarMovement.cs
@@ -3,38 +3,39 @@ using System.Collections.Generic;
 
 public class CarMovement : MonoBehaviour {
 
+    private static Quaternion directionalOffset = Quaternion.Euler(0, 270, 0);
+
     // Displayed in the editor
     public float speed = 1f;
     public float verticalOffset = 0f;
     public List<GameObject> waypoints;
 
     float progress = 0f;
-    int current = 0;
+    int nextWaypoint = 0;
 
     Vector3 target;
 
+    private void setNewTarget ()
+    {
+        // Setting the new target to the next entry in the target vector.
+        target = waypoints[nextWaypoint].transform.position;
+        target.y += verticalOffset;
+
+        // Performing the rotation
+        this.transform.rotation = Quaternion.LookRotation(target - this.transform.position) * directionalOffset;
+
+        // Updating next waypoint
+        if (++nextWaypoint >= waypoints.Count) { this.enabled = false; }
+    }
+
     // Use this for initialization
     void Start () {
-        target = waypoints[current].transform.position;
-        target.y += verticalOffset;
+        setNewTarget();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(this.transform.position);
-
-        if (current >= waypoints.Count)
-        {
-            this.enabled = false;
-            return;
-        }
-
         this.transform.position = Vector3.MoveTowards(this.transform.position, target, speed * Time.deltaTime);
-
-        if (Vector3.Distance(this.transform.position, target) < speed)
-        {
-            target = waypoints[++current].transform.position;
-            target.y += verticalOffset;
-        }
+        if (Vector3.Distance(this.transform.position, target) < speed) { setNewTarget(); }
     }
 }
