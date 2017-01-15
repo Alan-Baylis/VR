0a5d739ae3240f331b9ffbfbb64c2641cef4b608
Ari Marin <ari.aarno.kristian.marin@gmail.com>
2017-01-15 18:47:34 +0200

    Added duration of narration to the upper left corner of the sceen. Made prefab that should be put to AudioHandlerScript's public GameObject prefab, so it is instantieated properly.

--- Assets/Scripts/AudioHandlerScript.cs
+++ Assets/Scripts/AudioHandlerScript.cs
@@ -4,19 +4,24 @@ using UnityEngine.SceneManagement;
 
 public class AudioHandlerScript : MonoBehaviour
 {
+    public GameObject prefab;
 
     private AudioSource[] sources;
-
-    
-
+    private GameObject controller;
+    private GameObject counter;
 
     void Awake()
     {
     
-            //Get every single audio sources in the scene.
-            sources = GameObject.FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
-            Debug.Log("Sources initialized");
-        
+        //Get every single audio sources in the scene.
+        sources = GameObject.FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+        Debug.Log("Sources initialized");
+
+        controller = GameObject.Find("Controller (left)");
+        counter = (GameObject)Instantiate(prefab, transform.position, transform.rotation);
+        counter.transform.parent = controller.transform;
+        //counter.transform.position = controller.transform.position;
+
     }
 
     // Update is called once per frame
@@ -36,5 +41,44 @@ public class AudioHandlerScript : MonoBehaviour
                 audioSource.Stop();
             }
         }
+    }
+
+    // Function to know if narration is playing
+    public bool IsNarrationPlaying()
+    {
+        foreach (AudioSource audioSource in sources)
+        {
+            if (audioSource.isPlaying && audioSource.outputAudioMixerGroup.name == "Narrative")
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Function to get current time / lenght in string format
+    public string Duration()
+    {
+        if (IsNarrationPlaying())
+        {
+            return string.Format("{0:0.#}s / {1:0.#}s", PlayingSource().time, PlayingSource().clip.length);
+        }
+
+        return "";
+    }
+
+    // Get the source playing narration
+    private AudioSource PlayingSource()
+    {
+        foreach (AudioSource audioSource in sources)
+        {
+            if (audioSource.isPlaying && audioSource.outputAudioMixerGroup.name == "Narrative")
+            {
+                return audioSource;
+            }
+        }
+
+        return new AudioSource();
     }
 }
---
+++ Assets/Scripts/AudioTime.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class AudioTime : MonoBehaviour {
+
+    public int fontSize = 14;
+    private Text text;
+
+    // Use this for initialization
+    void Start () {
+        text = GetComponent<Text>();
+        text.fontSize = fontSize;
+        text.text = "";
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (text != null && GameObject.FindGameObjectWithTag("AudioHandler").GetComponent<AudioHandlerScript>().IsNarrationPlaying())
+        {
+            text.text = "Kesto: " + GameObject.FindGameObjectWithTag("AudioHandler").GetComponent<AudioHandlerScript>().Duration();
+        }
+        else
+        {
+            text.text = "";
+        }
+    }
+}
