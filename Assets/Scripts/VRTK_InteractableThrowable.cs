78b39de9b82570b3a38b2de831267354bfab5063
Arno <arno.lehtonen@gmail.com>
2017-01-17 19:26:20 +0200

    THE KNIFE IS THROWABLE AND HILARIOUS

---
+++ Assets/Scripts/VRTK_InteractableThrowable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace VRTK
+{
+    
+
+    public class VRTK_InteractableThrowable : VRTK_InteractableObject
+    {
+        public string CollidingWithTag;
+
+        public override void Ungrabbed(GameObject previousGrabbingObject)
+        {
+            //Overriding this for the throwing mechanics to be re-enabled
+            previousKinematicState = false;
+            base.Ungrabbed(previousGrabbingObject);
+
+        }
+
+
+       void OnCollisionEnter(Collision hit)
+        {
+            Debug.Log(hit.collider.tag);
+            if (hit.collider.tag == CollidingWithTag)
+            {
+                GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
+    }
+
+    
+}
