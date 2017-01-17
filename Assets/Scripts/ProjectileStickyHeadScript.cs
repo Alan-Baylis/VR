de5144442cd9fdcfabb71242bd0dd7d17ca59b7d
Arno <arno.lehtonen@gmail.com>
2017-01-17 22:14:45 +0200

    Better knife. Added scenes for all of the videos, feel free to add stuff there (text, items) if needed.

---
+++ Assets/Scripts/ProjectileStickyHeadScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileStickyHeadScript : MonoBehaviour {
+
+    //Add this to a projectile along with a box collider
+    void Start()
+    {
+        if(GetComponent<Collider>() != null)
+        {
+            Debug.Log("You need a collider to use this script.");
+        }
+    }
+
+    public string CollidingWithTag;
+
+    void OnTriggerEnter(Collider hit)
+    {
+        Debug.Log(hit.GetComponent<Collider>().tag);
+        if (hit.GetComponent<Collider>().tag == CollidingWithTag)
+        {
+            transform.parent.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+}
--- Assets/Scripts/VRTK_InteractableThrowable.cs
+++ Assets/Scripts/VRTK_InteractableThrowable.cs
@@ -7,25 +7,17 @@ namespace VRTK
 
     public class VRTK_InteractableThrowable : VRTK_InteractableObject
     {
-        public string CollidingWithTag;
 
         public override void Ungrabbed(GameObject previousGrabbingObject)
         {
-            //Overriding this for the throwing mechanics to be re-enabled
+            //Overriding this (hacky) for the throwing mechanics to be re-enabled
             previousKinematicState = false;
             base.Ungrabbed(previousGrabbingObject);
 
         }
 
 
-       void OnCollisionEnter(Collision hit)
-        {
-            Debug.Log(hit.collider.tag);
-            if (hit.collider.tag == CollidingWithTag)
-            {
-                GetComponent<Rigidbody>().isKinematic = true;
-            }
-        }
+
     }
 
     
