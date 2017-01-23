0087b0f8950cf433e822e13bdadd067e67d2483d
Ville Nukarinen <ville.nukarinen@gmail.com>
2017-01-23 20:41:11 +0200

    Add exit portals to road - Simple particle effect with rotating text - Takes the player back to investigation room on collision

---
+++ Assets/ExitScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+using UnityEngine.SceneManagement;
+
+public class ExitScript : MonoBehaviour {
+
+    public string SceneName;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponentInParent<VRTK_PlayerObject>().objectType == VRTK_PlayerObject.ObjectTypes.Headset)
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+}
