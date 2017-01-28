2caaa83bb2b7e07f08a23d312bb2c44a51901bcd
Arno <arno.lehtonen@gmail.com>
2017-01-28 22:34:17 +0200

    Bug fixes.

---
+++ Assets/Scripts/RoadSceneStartPosition.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace VRTK
+{
+   
+
+    public class RoadSceneStartPosition : MonoBehaviour
+    {
+
+        public int position = 0;
+
+        void Start()
+        {
+            GetComponent<Show_Details>().InteractableObjectUsed += new InteractableObjectEventHandler(SetStartingPosition);
+    
+    }
+
+  
+        void SetStartingPosition(object sender, InteractableObjectEventArgs e)
+        {
+            DataStore.setValue("road_position", position);
+        }
+    }
+}
